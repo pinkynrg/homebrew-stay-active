6b56048e60473d115fa6a1844cf896ff62b8e7e8
Francesco Meli <francesco.meli@beefree.io>
2024-08-28 09:32:20 +0200

    fix: update url and sha256

--- Formula/stay-active.rb
+++ Formula/stay-active.rb
@@ -1,17 +1,17 @@
-class Stayactive < Formula
+class StayActive < Formula
   desc "Keeps Slack online and Mac awake"
-  homepage "https://github.com/pinkynrg/slack-awake"
-  url "https://github.com/yourusername/stayactive/archive/refs/tags/v0.1.0.tar.gz" # Replace with your tarball URL
-  sha256 "your_sha256_checksum" # Replace with the actual SHA256 checksum
+  homepage "https://github.com/pinkynrg/homebrew-slack-awake"
+  url "https://github.com/pinkynrg/homebrew-stay-active/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "e4146ca565a8031c21ee992c484a130fd5a03d93bf2ff772bafaecd37d67bee4"
   license "MIT"
 
-  depends_on "cliclick"  # This ensures cliclick is installed
+  depends_on "cliclick"
 
   def install
-    bin.install "stay-active.sh" => "stayactive"
+    bin.install "src/stay-active.sh" => "stay-active"
   end
 
   test do
-    system "#{bin}/stayactive", "--version"
+    system "#{bin}/stay-active", "--version"
   end
 end
