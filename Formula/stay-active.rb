39d92ef93318a793e4812965dace776289f61472
Francesco Meli <francesco.meli@beefree.io>
2024-08-28 10:03:11 +0200

    fix: new version

--- Formula/stay-active.rb
+++ Formula/stay-active.rb
@@ -1,8 +1,8 @@
 class StayActive < Formula
   desc "Keeps Slack online and Mac awake"
   homepage "https://github.com/pinkynrg/homebrew-slack-awake"
-  url "https://github.com/pinkynrg/homebrew-stay-active/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "e4146ca565a8031c21ee992c484a130fd5a03d93bf2ff772bafaecd37d67bee4"
+  url "https://github.com/pinkynrg/homebrew-stay-active/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "4ca5665ef2910b17d737c9da405ad16c333d4fe6b0a7316715e6aa19fb1d6b67"
   license "MIT"
 
   depends_on "cliclick"
